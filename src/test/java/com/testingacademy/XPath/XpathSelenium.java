2364118e579924496e9b87b0cda7e59aa3daec08
Aditi Soni <aditininawe9@gmail.com>
2026-03-01 15:43:27 +0530

    xpath

--- src/test/java/com/testingacademy/XPath/XpathSelenium.java
+++ src/test/java/com/testingacademy/XPath/XpathSelenium.java
@@ -1,4 +1,38 @@
 package com.testingacademy.XPath;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class XpathSelenium {
+    @Owner("Aditi")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify login is working")
+    @Test
+    public void test_OHRM_login() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://awesomeqa.com/hr/web/index.php/auth/login");
+        Thread.sleep(5000);
+
+        WebElement input_username = driver.findElement(By.xpath("//input[@name='username']"));
+        WebElement input_password = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        WebElement submitBtn = driver.findElement(By.xpath("//button"));
+
+        input_username.sendKeys("admin");
+        input_password.sendKeys("Hacker@4321");
+        submitBtn.click();
+
+        Thread.sleep(5000);
+        WebElement h6 = driver.findElement(By.xpath("//span[@class='oxd-topbar-header-breadcrumb']"));
+        Assert.assertEquals(h6.getText(),"PIM");
+
+        driver.quit();
+    }
 }
