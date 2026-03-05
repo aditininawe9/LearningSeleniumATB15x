f2c56c8f3f3ab83cd1e2d27582cc427ab973d906
Aditi Soni <aditininawe9@gmail.com>
2026-03-05 06:48:43 +0530

    xpath axes

--- src/test/java/com/testingacademy/XPath_Axes/TestSelenium_XpathAxes.java
+++ src/test/java/com/testingacademy/XPath_Axes/TestSelenium_XpathAxes.java
@@ -6,10 +6,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import static org.openqa.selenium.By.xpath;
+
 public class TestSelenium_XpathAxes {
     @Test
     @Description("Test case Description")
-    public void webTables() {
+    public void webTables() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://awesomeqa.com/webtable.html");
         driver.manage().window().maximize();
@@ -22,12 +24,19 @@ public class TestSelenium_XpathAxes {
         String second_part = "]/td[";
         String third_part = "]";
 
-        Integer row = driver.findElements(By.xpath("//table[@id=\"customers\"]/tbody/tr")).size();
-        Integer column = driver.findElements(By.xpath("//table[@id=\"customers\"]/tbody/td[2]")).size();
-        for(int i = 1; i <= row; i++){
+        Integer row = driver.findElements(xpath("//table[@id='customers']/tbody/tr")).size();
+        Integer column = driver.findElements(xpath("//table[@id=\"customers\"]/tbody/tr[2]/td")).size();
+        System.out.println("row "+row+ " column "+column);
+        for(int i = 2; i <= row; i++){
             for (int j = 1; j <= column; j++) {
                 String dynamic_path = first_part+i+second_part+j+third_part;
-                System.out.println("-----path---"+dynamic_path);
+                String data = driver.findElement(xpath(dynamic_path)).getText();
+                System.out.println(data);
+                if (data.contains("Helen")) {
+                    String country_path = dynamic_path + "/following-sibling::td";
+                    String country_path_text = driver.findElement(xpath(country_path)).getText();
+                    System.out.println(country_path_text);
+                }
             }
         }
     }
